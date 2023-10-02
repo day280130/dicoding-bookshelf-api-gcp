b2336aeda96d543c4b07176b7158df1b191ec770
DAY280130 <dimas.280130@gmail.com>
2023-10-03 02:20:23 +0700

    feat: routes activated

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { server } from "@src/configs/server.js";
+import { routes } from "@src/routes/index.js";
 import "dotenv-safe/config.js";
 
-// server.route(routes);
+server.route(routes);
 
 await server.start();
 console.log(`Server berjalan pada ${server.info.uri}`);
