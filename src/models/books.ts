cbae96b283f225d253cbb6197e50e6b6c29b9c2c
DAY280130 <dimas.280130@gmail.com>
2023-10-04 00:48:30 +0700

    feat: added uuid validation

--- src/models/books.ts
+++ src/models/books.ts
@@ -1,49 +1,51 @@
 import { z } from "zod";
 
 export const bookSchema = z.object({
-  id: z.string({
-    invalid_type_error: "id must be string",
-    required_error: "id required",
-  }),
+  id: z
+    .string({
+      invalid_type_error: "id have to be a string",
+      required_error: "id required",
+    })
+    .uuid("id have to be a uuid"),
   name: z.string({
-    invalid_type_error: "id must be string",
+    invalid_type_error: "id have to be a string",
     required_error: "id required",
   }),
   year: z.coerce
     .number({
-      invalid_type_error: "year must be number",
+      invalid_type_error: "year have to be a number",
       required_error: "year required",
     })
     .gte(0, "year cannot be negative"),
   author: z.string({
-    invalid_type_error: "author must be string",
+    invalid_type_error: "author have to be a string",
     required_error: "author required",
   }),
   summary: z.string({
-    invalid_type_error: "summary must be string",
+    invalid_type_error: "summary have to be a string",
     required_error: "summary required",
   }),
   publisher: z.string({
-    invalid_type_error: "publisher must be string",
+    invalid_type_error: "publisher have to be a string",
     required_error: "publisher required",
   }),
   pageCount: z.coerce
     .number({
-      invalid_type_error: "pageCount must be number",
+      invalid_type_error: "pageCount have to be a number",
       required_error: "pageCount required",
     })
     .gte(0, "pageCount cannot be negative"),
   readPage: z.coerce
     .number({
-      invalid_type_error: "readPage must be number",
+      invalid_type_error: "readPage have to be a number",
       required_error: "readPage required",
     })
     .gte(0, "readPage cannot be negative"),
   finished: z.coerce.boolean({
-    invalid_type_error: "finished must be booleanish value",
+    invalid_type_error: "finished have to be a booleanish value",
   }),
   reading: z.coerce.boolean({
-    invalid_type_error: "reading must be booleanish value",
+    invalid_type_error: "reading have to be a booleanish value",
   }),
   insertedAt: z.string().datetime(),
   updatedAt: z.string().datetime(),
